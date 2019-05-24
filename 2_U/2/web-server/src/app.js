059e71e86e383aaad2e1ff55157a2e40b6540f36
Ivo Costa <costa_ivo@yahoo.com>
2019-05-25 00:45:31 +0530

    added assets

--- 2_U/2/web-server/src/app.js
+++ 2_U/2/web-server/src/app.js
@@ -7,23 +7,23 @@ const app = express()
 const publicDirectory = path.join(__dirname,'../public')
 app.use(express.static(publicDirectory))
 
-// myWebsite.com
-app.get('/',(req,res)=>{
-res.send('<h1>Hello World</h1>')
-})
+// // myWebsite.com
+// app.get('/',(req,res)=>{
+// res.send('<h1>Hello World</h1>')
+// })
 
-// myWebsite.com/about
-app.get('/about',(req,res)=>{
-    res.send('<h1>About Page</h1> <br> <h3>check the weather in your locality</h3>')
-})
+// // myWebsite.com/about
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About Page</h1> <br> <h3>check the weather in your locality</h3>')
+// })
 
-// myWebsite.com/help
-app.get('/help',(req,res)=>{
-    res.send({
-        name:'ivo costa',
-        message:'i am here to help you'
-    })
-})
+// // myWebsite.com/help
+// app.get('/help',(req,res)=>{
+//     res.send({
+//         name:'ivo costa',
+//         message:'i am here to help you'
+//     })
+// })
 
 // myWebsite.com/weather
 app.get('/weather',(req,res)=>{
