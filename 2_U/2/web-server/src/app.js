06cb1864ac8831d6a9ed0b740203ed8afb4f2a25
Ivo Costa <costa_ivo@yahoo.com>
2019-05-25 01:43:08 +0530

    webserver basic

--- 2_U/2/web-server/src/app.js
+++ 2_U/2/web-server/src/app.js
@@ -1,29 +1,38 @@
 const path = require('path')
 
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+//Define path for Express config
 const publicDirectory = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'/templates/views')
+const partialsPath = path.join(__dirname,'/templates/partials')
+
+
+// Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup for static directory to serve
 app.use(express.static(publicDirectory))
 
-// // myWebsite.com
-// app.get('/',(req,res)=>{
-// res.send('<h1>Hello World</h1>')
-// })
+// myWebsite.com
+app.get('/',(req,res)=>{
+res.render('index',{title:'Weather App 2.0',author:'Ivo Costa'})
+})
 
-// // myWebsite.com/about
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>About Page</h1> <br> <h3>check the weather in your locality</h3>')
-// })
+// myWebsite.com/about
+app.get('/about',(req,res)=>{
+    res.render('about',{title:'Weather App 2.0',author:'Ivo Costa'})
+})
 
-// // myWebsite.com/help
-// app.get('/help',(req,res)=>{
-//     res.send({
-//         name:'ivo costa',
-//         message:'i am here to help you'
-//     })
-// })
+// myWebsite.com/help
+app.get('/help',(req,res)=>{
+    res.render('help',{title:'Weather App 2.0',author:'Ivo Costa'})
+})
 
 // myWebsite.com/weather
 app.get('/weather',(req,res)=>{
