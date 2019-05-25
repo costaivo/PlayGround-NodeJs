16a9aba7148c6d9fd26f1469d8d5d07f0a2a9ff4
Ivo Costa <costa_ivo@yahoo.com>
2019-05-25 15:54:53 +0530

    add form for weather app

--- 2_U/2/web-server/public/js/app.js
+++ 2_U/2/web-server/public/js/app.js
@@ -4,9 +4,13 @@ console.log('javascript file was loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const msgOne = document.querySelector('#msg-one')
+const msgTwo = document.querySelector('#msg-two')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
+    msgOne.textContent='loading...'
+    msgTwo.textContent=''
 
     const location = search.value
 
@@ -16,9 +20,14 @@ weatherForm.addEventListener('submit',(e)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
+
+            msgOne.textContent=data.error
         }else{
             console.log(data.location)
             console.log(data.forecast)
+    
+            msgOne.textContent=data.forecast.todayForecast
+            msgTwo.textContent=data.forecast.temperature
         }
     })
 })
