7fbbf278fbb0789d185757aec43f480c5e6a0c92
Ivo Costa <costa_ivo@yahoo.com>
2019-08-15 00:20:09 +0530

    updated Application to show words progress

--- spellingBee/src/app.js
+++ spellingBee/src/app.js
@@ -46,7 +46,7 @@ const _addVoicesList = voices => {
 //     'received'
 // ];
 
-const _wordsToSpell = [
+let _wordsToSpell = [
   'adventure',
   'structure',
   'mysterious',
@@ -69,22 +69,44 @@ const _wordsToSpell = [
   'camouflage',
   'recommend',
   'mathematician',
-'announcement',
-'plateau',
-'sculpture',
-'humorous',
-'perseverance',
-'interrogation',
-'successful',
-'musician'
+  'announcement',
+  'plateau',
+  'sculpture',
+  'humorous',
+  'perseverance',
+  'interrogation',
+  'successful',
+  'musician'
 ];
 
-let scoreCount =0;
-let wordIndex=0;
-let totalIncorrectAttempts=0;
-let currentIncorrectAttempts=0;
+let scoreCount = 0;
+let wordIndex = 0;
+let totalIncorrectAttempts = 0;
+let currentIncorrectAttempts = 0;
+let totalWordsAttempted = 0;
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+
+  return array;
+}
 
 function _init() {
+  _wordsToSpell = shuffle(_wordsToSpell)
   const speech = new Speech();
   speech
     .init({
@@ -108,9 +130,9 @@ function _init() {
     .catch(e => {
       console.error("An error occured while initializing : ", e);
     });
-  const text = speech.hasBrowserSupport()
-    ? "Hurray, your browser supports speech synthesis"
-    : "Your browser does NOT support speech synthesis. Try using Chrome of Safari instead !";
+  const text = speech.hasBrowserSupport() ?
+    "Hurray, your browser supports speech synthesis" :
+    "Your browser does NOT support speech synthesis. Try using Chrome of Safari instead !";
   document.getElementById("support").innerHTML = text;
 }
 
@@ -119,10 +141,10 @@ function _prepareSpeakButton(speech) {
   const pauseButton = document.getElementById("pause");
   const resumeButton = document.getElementById("resume");
   const startTrainingButton = document.getElementById("startMyTraining");
-  const submitAnswerButton=document.getElementById("submitAnswer");
-  const inputAnswer=document.getElementById("inputAnswer");
-const mainHeader=document.getElementById("mainHeader");
-const trainingArena=document.getElementById("trainingArena");
+  const submitAnswerButton = document.getElementById("submitAnswer");
+  const inputAnswer = document.getElementById("inputAnswer");
+  const mainHeader = document.getElementById("mainHeader");
+  const trainingArena = document.getElementById("trainingArena");
   const textarea = document.getElementById("text");
   const languages = document.getElementById("languages");
 
@@ -130,8 +152,14 @@ const trainingArena=document.getElementById("trainingArena");
 
   const lblIncorrectAttempts = document.getElementById("wrongAttempts");
   const currentWrongAttempts = document.getElementById("currentWrongAttempts");
+  const lblTotalWordsAttempted = document.getElementById("totalWordsAttempted");
+  const lblTotalWordsAvailable = document.getElementById("totalWordsAvailable");
+  const lblScoreDivider = document.getElementById("scoreDivider");
+  const divWordsDisplayArea = document.getElementById("wordsDisplayArea");
+  const progressBar = document.getElementById("progressBar");
+  const progressCounter = document.getElementById("progressCounter");
   
-  let currentWordToSpell = "cat";
+  let currentWordToSpell = _wordsToSpell[0];
 
   speakButton.addEventListener("click", () => {
     const language = languages.value;
@@ -155,16 +183,16 @@ const trainingArena=document.getElementById("trainingArena");
           onboundary: event => {
             console.log(
               event.name +
-                " boundary reached after " +
-                event.elapsedTime +
-                " milliseconds."
+              " boundary reached after " +
+              event.elapsedTime +
+              " milliseconds."
             );
           }
         }
       })
       .then(data => {
         console.log("Success !", data);
-        mainHeader.hidden=true;
+        mainHeader.hidden = true;
         trainingArena.classList.remove("d-none")
       })
       .catch(e => {
@@ -180,110 +208,185 @@ const trainingArena=document.getElementById("trainingArena");
     speech.resume();
   });
 
-  submitAnswerButton.addEventListener("click",()=>{
-    if(inputAnswer.value.toLowerCase()===currentWordToSpell.toLocaleLowerCase())
-    {
-        if(scoreCount % 2 === 0){
-            console.log('odd score');
-        speech
-      .speak({
-        text: "Very Good my Friend. Now for the next word",
-        queue:false});
+  submitAnswerButton.addEventListener("click", () => {
+    console.log("Submit Button Clicked!!")
 
-        setTimeout({},2000);
-      }
-      else{
-        console.log('even score');
-        speech
-        .speak({
-          text: "Great Going buddy. Your current score is :"+scoreCount+"points. Next word coming up",
-          queue:false})
-          setTimeout({},2000);
-      }
-      var currentMarks = (currentIncorrectAttempts*-1)+10;
+    if (inputAnswer.value.toLowerCase() === currentWordToSpell.toLocaleLowerCase()) {
+      var currentMarks = (currentIncorrectAttempts * -1) + 10;
 
-      if(currentMarks>0)
+      if (currentMarks > 0)
         scoreCount += currentMarks;
       else
-        scoreCount +=1;
+        scoreCount += 1;
 
-        scoreLabel.innerText=scoreCount;
-        setNextWord();
-    }
-    else{
+      if (scoreCount % 2 == 0) {
+        console.log('odd score');
+        speech
+          .speak({
+            text: "Very Good my Friend. Now for the next word",
+            queue: false
+          });
+
+        setTimeout({}, 2000);
+      } else {
+        console.log('even score');
+        speech
+          .speak({
+            text: "Great Going buddy. Your current score is :" + scoreCount + "points. Next word coming up",
+            queue: false
+          })
+        setTimeout({}, 2000);
+      }
+  
+
+      scoreLabel.innerText = scoreCount;
+      setNextWord();
+    } else {
       currentIncorrectAttempts++;
       currentWrongAttempts.innerText = currentIncorrectAttempts;
 
-      if(currentIncorrectAttempts<=2)
-      {
+      if (currentIncorrectAttempts < 2) {
         speech
-        .speak({
-          text: "Sorry That was an Incorrect Answer. Try Again!",
-          queue:false})
+          .speak({
+            text: "Sorry That was an Incorrect Answer. Try Again!",
+            queue: false
+          })
 
-          inputAnswer.style.backgroundColor = "red";
-          inputAnswer.style.color="white";
-       }
-        else{
-          speech
+        inputAnswer.style.backgroundColor = "red";
+        inputAnswer.style.color = "white";
+      } else {
+        speech
           .speak({
             text: "Sorry! That was a wrong spelling. Try the next word now",
-            queue:false})
-            setTimeout(setNextWord,5000)
-        }
+            queue: false
+          })
+        setTimeout(setNextWord, 5000)
+      }
 
     }
 
   });
 
-  function setNextWord(){
+  // Get the input field
+var input = document.getElementById("inputAnswer");
 
-    if(currentIncorrectAttempts>0)
-    {
-      totalIncorrectAttempts++;
-      lblIncorrectAttempts.innerText= totalIncorrectAttempts;
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("submitAnswer").click();
+  }
+});
+
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      let temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+
+  return array;
+}
+
+  function setNextWord() {
+   
+    if (totalWordsAttempted === 0) {
+      lblTotalWordsAttempted.classList.remove("d-none")
+      lblTotalWordsAvailable.classList.remove("d-none")
+      progressBar.classList.remove("d-none")
+      lblScoreDivider.classList.remove("d-none")
+      progressCounter.setAttribute("aria-valuemax", 0)
+      lblTotalWordsAvailable.innerText = _wordsToSpell.length
     }
-    currentIncorrectAttempts=0;
-    currentWrongAttempts.innerText="0";
+    totalWordsAttempted++;
+    lblTotalWordsAttempted.innerText = totalWordsAttempted;
+    
+    const currentWord = _wordsToSpell[ wordIndex]
+    progressCounter.setAttribute("aria-valuenow", totalWordsAttempted)
+
+    const percentageComplete = (totalWordsAttempted*100/_wordsToSpell.length);
+    const progress ="width: "+percentageComplete+"%";
+
+    progressCounter.setAttribute("style",progress)
+
+    if(currentIncorrectAttempts > 0) {
+      totalIncorrectAttempts++;
+      lblIncorrectAttempts.innerText = totalIncorrectAttempts;
+      divWordsDisplayArea.innerHTML+="<label class='btn btn-danger'>"+currentWord+" <span class='badge badge-danger badge-pill'><i class='fa fa-times-circle' aria-hidden='true'></i></span></label>"
+    }
+    else
+    {
+      divWordsDisplayArea.innerHTML+="<label class='btn btn-primary'>"+currentWord+" <span class='badge badge-success badge-pill'><i class='fa fa-check' aria-hidden='true'></i></span></label>"
+    }
+    currentIncorrectAttempts = 0;
+    currentWrongAttempts.innerText = "0";
+
     inputAnswer.value = "";
     inputAnswer.style.backgroundColor = "white";
-    inputAnswer.style.color="black"
+    inputAnswer.style.color = "black"
 
-    if(totalIncorrectAttempts>3)
-    {
+  
+
+    
+     
+
+   
+
+  
+
+    if (totalIncorrectAttempts > 3) {
+      
       speech
-      .speak({
-        text: `Sorry! You have Exceeded all the lifelines. Your Final Score is ${scoreCount}. Please restart to try again.`,
-        queue:false})
-    }
-    else  if(wordIndex>=_wordsToSpell.length)
-      {
-        speech
         .speak({
-          text: "You have completed Training for all the words today. Your Final score is "+scoreCount+" points",
-          queue:false})
-      }
-      else
-      {
-        currentWordToSpell=_wordsToSpell[wordIndex];
-        speech
-        .speak({
-          text: "Ready for Next Word..., spell "+currentWordToSpell,
-          queue:false})
-      }
-    wordIndex++;
-  };
-  startTrainingButton.addEventListener("click",()=>{
+          text: `Sorry! You have Exceeded all the lifelines. Your Final Score is ${scoreCount}. Please restart to try again.`,
+          queue: false
+        })
+        setTimeout(setNextWord, 2000)
 
-    submitAnswer.disabled=false;
+    } else if (wordIndex >= _wordsToSpell.length) {
+      speech
+        .speak({
+          text: "You have completed Training for all the words today. Your Final score is " + scoreCount + " points",
+          queue: false
+        })
+    } else {
+        //move to next word
+    wordIndex++;
+      currentWordToSpell = _wordsToSpell[wordIndex];
+      speech
+        .speak({
+          text: "Ready for Next Word..., spell " + currentWordToSpell,
+          queue: false
+        })
+    
+  
+    }
+    
+  };
+  startTrainingButton.addEventListener("click", () => {
+
+    submitAnswer.disabled = false;
     speech
       .speak({
-        text: "Spell "+currentWordToSpell,
+        text: "Spell " + currentWordToSpell,
         queue: false,
         listeners: {
           onstart: () => {
-          
-            startTrainingButton.value ="Play Word";
+
+            startTrainingButton.value = "Play Word";
             console.log("Start utterance");
           },
           onend: () => {
@@ -295,9 +398,9 @@ const trainingArena=document.getElementById("trainingArena");
           onboundary: event => {
             console.log(
               event.name +
-                " boundary reached after " +
-                event.elapsedTime +
-                " milliseconds."
+              " boundary reached after " +
+              event.elapsedTime +
+              " milliseconds."
             );
           }
         }
@@ -309,7 +412,7 @@ const trainingArena=document.getElementById("trainingArena");
       .catch(e => {
         console.error("An error occurred :", e);
       });
-   
+
   })
 }
 
